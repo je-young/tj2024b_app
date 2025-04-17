efff603f6d622e6f724886cc376c28dd818c48c7
je-young <jey2965@gmail.com>
2025-04-17 15:37:51 +0900

    수업 app/layout/member

---
+++ lib/app/main.dart
@@ -0,0 +1,7 @@
+// * main.dart : 플러터 실행 하는 파일
+import 'package:flutter/material.dart';
+import 'package:flutter_study/app/layout/myapp.dart';
+void main(){
+  // runApp( 최초실행할위젯() );최초 위젯을 실행 하는 함수
+  runApp( MyApp() ); // MaterialApp 갖는 위젯
+}
